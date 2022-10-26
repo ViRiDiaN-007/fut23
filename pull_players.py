d11f6e6a627e1de14e40030c0de0eb09915dfb1a
Viri <trent2017@gmail.com>
2022-10-26 15:15:35 -0400

    First try

---
+++ pull_players.py
@@ -0,0 +1,11 @@
+import requests
+import json
+import unicodedata
+
+url = "https://fifa23.content.easports.com/fifa/fltOnlineAssets/23DF3AC5-9539-438B-8414-146FAFDE3FF2/2023/fut/items/web/players.json"
+
+resp = requests.get(url=url)
+pretty_response = json.loads(unicodedata.normalize('NFKD', resp.text).encode('ascii', 'ignore'))
+json_players = json.dumps(pretty_response, indent=4)
+with open("players.json",'w') as players_json:
+    players_json.write(json_players)
